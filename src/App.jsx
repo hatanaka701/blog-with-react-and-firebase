68ab67531dffc3ccea18360f885016285b769252
humanmodeling OIT <humanmodelingoit@humanmodelingMacBook-Air-2.local>
2023-02-23 14:33:42 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
+import uuid from "react-uuid";
+
+function App() {
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem("notes") || [])
+  );
+  const [activeNote, setActiveNote] = useState(false);
+
+  useEffect(() => {
+    // ローカルストレージにノートを保存する
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  useEffect(() => {
+    setActiveNote(notes[0].id);
+  }, []);
+
+  const onAddNote = () => {
+    console.log("新しくノートが追加されました");
+    const newNote = {
+      id: uuid(),
+      title: "新しいノート",
+      content: "",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  };
+
+  const onDeleteNote = (id) => {
+    // note.idとidが等しくないものを返す
+    const filterNotes = notes.filter((note) => note.id !== id);
+    setNotes(filterNotes);
+  };
+
+  const getActiveNote = () => {
+    // 計算式などはreturnいる、定義はいらない
+    // idが一致したnoteを返してる
+    return notes.find((note) => note.id === activeNote);
+  };
+
+  const onUpdateNote = (updatedNote) => {
+    // 修正された新しいノートの配列を返す
+    const updateNotesArray = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote;
+      } else {
+        return note;
+      }
+    });
+    setNotes(updateNotesArray);
+  };
+
+  return (
+    <div className="App">
+      <Sidebar
+        onAddNote={onAddNote}
+        notes={notes}
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
+    </div>
+  );
+}
+
+export default App;
